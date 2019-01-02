93046f03cb0246d37e6fa8ad239eef7b2a6b8a90
David Dinh <dmdinh22@gmail.com>
2019-01-01 22:20:47 -0800

    added asset and constraints for asset

--- lbta-autolayout/ViewController.swift
+++ lbta-autolayout/ViewController.swift
@@ -12,7 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // here's the entry point into our app
+//        view.backgroundColor = .yellow
+        
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
+        view.addSubview(imageView)
+//        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        
+        // this enables autolayout for the image view
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true // anchor to view's topAnchor with 100pts padding
+        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
     }
 
 
