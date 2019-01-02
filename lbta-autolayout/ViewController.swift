0520373ea10d3c9f921a5c2470f5b5d47ca064ca
David Dinh <dmdinh22@gmail.com>
2019-01-02 01:02:04 -0800

    refactor to use attributedText, add paragraph of text

--- lbta-autolayout/ViewController.swift
+++ lbta-autolayout/ViewController.swift
@@ -25,8 +25,13 @@ class ViewController: UIViewController {
     
     let descriptionTextView: UITextView = {
         let textView = UITextView()
-        textView.text = "Join us today in our fun and games!"
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
+//        textView.text = "Join us today in our fun and games!"
+//        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+        
+        attributedText.append(NSAttributedString(string: "\n\n\nAre you ready for loads and loads of fun Don't wait any longer! We hope to see you in our stores soon.", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13), NSAttributedString.Key.foregroundColor: UIColor.gray]))
+        
+        textView.attributedText = attributedText
         textView.textAlignment = .center
         textView.isEditable = false
         textView.isScrollEnabled = false
@@ -39,7 +44,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // entry pt into app
-//        view.addSubview(bearImageView)
         view.addSubview(descriptionTextView)
         
         setupLayout()
@@ -54,24 +58,17 @@ class ViewController: UIViewController {
         topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-//        topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-//        topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true // multiplier sets container size to half of screen height
         
         topImageContainerView.addSubview(bearImageView)
         
         bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
         bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
-        bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.6).isActive = true
+        bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.6).isActive = true // scale img height to container height
         
-//        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true // anchor to view's topAnchor with 100pts padding
-//        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-//        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        
-        descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 150).isActive = true // pin to bottom of bear img
-        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true // pin to bottom of top image container
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24).isActive = true
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
     
