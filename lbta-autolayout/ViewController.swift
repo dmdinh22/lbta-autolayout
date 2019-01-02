ff9402f32f3855d3fc95200ab2a28ba9645213ce
David Dinh <dmdinh22@gmail.com>
2019-01-02 00:43:56 -0800

    create top half container

--- lbta-autolayout/ViewController.swift
+++ lbta-autolayout/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
         
         // this enables autolayout for the image view
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // images width and height stays same regardless of imageView size
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
@@ -36,17 +39,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // entry pt into app
-        view.addSubview(bearImageView)
+//        view.addSubview(bearImageView)
         view.addSubview(descriptionTextView)
         
         setupLayout()
     }
 
     private func setupLayout() {
-        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true // anchor to view's topAnchor with 100pts padding
-        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        // create container for top half
+        let topImageContainerView = UIView()
+        view.addSubview(topImageContainerView)
+        
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false // enable autolayout
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+//        topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//        topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true // multiplier sets container size to half of screen height
+        
+        topImageContainerView.addSubview(bearImageView)
+        
+        bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.6).isActive = true
+        
+//        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true // anchor to view's topAnchor with 100pts padding
+//        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+//        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
         
         descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 150).isActive = true // pin to bottom of bear img
         descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
