9a820d540e9896459507386ba64714de02b71375
David Dinh <dmdinh22@gmail.com>
2019-01-07 01:16:37 -0800

    migrate bottom controls into swipe controller

--- lbta-autolayout/SwipingController.swift
+++ lbta-autolayout/SwipingController.swift
@@ -9,9 +9,41 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         Page(imageName: "heart_second", headerText: "Subscribe and get coupons on our daily events", bodyText: "Get notified of the savings immediately when we announce them on our website. Make sure to also give us any feedback you have."),
         Page(imageName: "leaf_third", headerText: "VIP members special services", bodyText: "")
     ]
+
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+
+        return button
+    }()
+
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.customPink, for: .normal)
+
+        return button
+    }()
+
+    private let pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.currentPage = 0
+        pageControl.numberOfPages = 4
+        pageControl.currentPageIndicatorTintColor = .customPink
+        pageControl.pageIndicatorTintColor = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
+
+        return pageControl
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupBottomControls()
         
         collectionView?.backgroundColor = .white
         collectionView.register(PageCell.self, forCellWithReuseIdentifier: "cellId")
@@ -43,6 +75,23 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         // set each collection view cell's size
         return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+
+    fileprivate func setupBottomControls() {
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
+        view.addSubview(bottomControlsStackView)
+
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlsStackView.distribution = .fillEqually
+
+        // alternative to setting constraints and not having to set .isActive = true
+        NSLayoutConstraint.activate([
+            // using safeAreaLayoutGuide to not be in the phone's safe area - iPhone X+
+            bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+            ])
     }
 }
 
